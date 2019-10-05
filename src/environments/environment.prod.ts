20ccfb95e6f4a5aa71d148ac97ae364626c832ef
Deni Chan <dchan@Denis-MacBook-Pro.local>
2019-10-05 13:08:15 +0300

    fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  githubAuthUrl: 'https://github.com/login/oauth/authorize/?client_id=134cf2f85b34e84d79c5&scope=repo'
+  githubAuthUrl: 'https://github.com/login/oauth/authorize/?client_id=242f26fee7e768e5b64e&scope=repo'
 };
